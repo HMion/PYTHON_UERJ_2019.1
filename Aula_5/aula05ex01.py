d0406dbc5dbe23595e2ef96f64e886a3bb5a36c8
Hamilton_Mion <48730459+HMion@users.noreply.github.com>
2019-04-30 17:19:43 -0300

    Create aula05ex01.py

---
+++ Aula_5/aula05ex01.py
@@ -0,0 +1,8 @@
+def Maximo(a,b,c):
+    t = max(a, b, c)
+    print("O maior valor das 03 entradas:", t)
+###############################################################################
+x = int(input("Entre com o primeiro número inteiro:"))
+y = int(input("Entre com o segundo número inteiro:"))
+z = int(input("Entre com o terceiro número inteiro:"))
+Maximo(x,y,z)
